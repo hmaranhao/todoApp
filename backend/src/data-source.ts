cd04ea4d001c164ce4858e1f4b86b9e97761c854
hmaranhao <herculeslima.maranhao@gmail.com>
2022-07-24 21:58:15 -0300

    create script start

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -7,11 +7,6 @@ console.log({ env: process.env.DATABASE_URL })
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
-    // host: process.env.POSTGRES_HOST ?? "localhost",
-    // port: 5432,
-    // username: "postgres",
-    // password: "12345",
-    // database: "todo_app",
     synchronize: true,
     logging: false,
     entities: [Todo],
