87a00a6805aa0647d9794fce5ef52f77579bf5fe
hmaranhao <herculeslima.maranhao@gmail.com>
2022-07-24 21:45:40 -0300

    adjusts to deploy backend

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,15 +2,16 @@ import { DataSource } from "typeorm"
 
 import { Todo } from "./domain/entities/todo/Todo"
 
-console.log({ env: process.env.POSTGRES_HOST })
+console.log({ env: process.env.DATABASE_URL })
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.POSTGRES_HOST ?? "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "12345",
-    database: "todo_app",
+    url: process.env.DATABASE_URL,
+    // host: process.env.POSTGRES_HOST ?? "localhost",
+    // port: 5432,
+    // username: "postgres",
+    // password: "12345",
+    // database: "todo_app",
     synchronize: true,
     logging: false,
     entities: [Todo],
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ AppDataSource.initialize().then(async () => {
     
     app.use(routes)
 
-    app.listen(5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
         console.log('Server is running on port', 5000)
     })
 }).catch(error => console.log(error))
